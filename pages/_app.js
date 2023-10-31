bf0d3182e6816dba08bd154527666a105a49e43b
eduardofleck <edusfleck@gmail.com>
2023-10-31 19:15:57 +0100

    Red colors

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,9 @@ import { I18nextProvider } from "react-i18next";
 import i18n from "../src/app/components/i18n";
 import { withTranslation, Trans } from "react-i18next";
 import TemplateManager from "../src/app/components/TemplateManager";
+import { red, white } from "@mui/material/colors";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
 import {
   ApolloClient,
   InMemoryCache,
@@ -14,13 +17,22 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
+const theme = createTheme({
+  palette: {
+    primary: red,
+    secondary: white,
+  },
+});
+
 function MyApp({ Component, pageProps }) {
   return (
     <I18nextProvider i18n={i18n}>
       <ApolloProvider client={client}>
-        <TemplateManager>
-          <Component i18n={i18n} {...pageProps} />
-        </TemplateManager>
+        <ThemeProvider theme={theme}>
+          <TemplateManager>
+            <Component i18n={i18n} {...pageProps} />
+          </TemplateManager>
+        </ThemeProvider>
       </ApolloProvider>
     </I18nextProvider>
   );
